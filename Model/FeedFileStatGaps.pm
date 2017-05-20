bf77d578ed54027386a0fdb72cb55c9ebae6ea70
Ivan <vanyabrovary@users.noreply.github.com>
2017-05-21 02:42:36 +0300

    Update FeedFileStatGaps.pm

--- Model/FeedFileStatGaps.pm
+++ Model/FeedFileStatGaps.pm
@@ -2,9 +2,8 @@ package Model::FeedFileStatGaps;
 
 use warnings; use strict;
 
-use XMLData::Model;
-our @ISA = qw/XMLData::Model/;
-
+use Model;
+our @ISA = qw/Model/;
 sub db_table() 	 { 'feed_file_stat_gaps' };
 sub db_columns() { qw/id feed_file_stat_id rcv_time_after rcv_time_till/ };
 
